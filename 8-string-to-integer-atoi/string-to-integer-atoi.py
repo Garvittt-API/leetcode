7acbb866460fb35808d60b8b93a5ac827020f1b0
Garvittt.exe <garvitchoudhary2315@gmail.com>
2026-06-27 17:51:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (13.20%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,39 +1,27 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        # Define 32-bit signed integer boundaries
-        INT_MIN, INT_MAX = -2**31, 2**31 - 1
-        
-        n = len(s)
-        i = 0
-        
-        # Step 1: Ignore leading whitespace
-        while i < n and s[i] == ' ':
-            i += 1
-            
-        # If we reached the end of the string, return 0
-        if i == n:
+        s = s.strip()  # Remove leading/trailing spaces
+        if not s:
             return 0
-            
-        # Step 2: Determine signedness
-        sign = 1
-        if s[i] == '+':
-            i += 1
-        elif s[i] == '-':
+
+        sign, i, res = 1, 0, 0
+
+        # Check for sign
+        if s[0] == '-':
             sign = -1
             i += 1
-            
-        # Step 3: Conversion & Step 4: Rounding (Overflow handling)
-        res = 0
-        while i < n and s[i].isdigit():
-            digit = int(s[i])
-            
-            # Check for overflow before multiplying
-            # If res > INT_MAX // 10, multiplying by 10 will definitely overflow.
-            # If res == INT_MAX // 10, adding a digit greater than 7 (or 8 for negative) overflows.
-            if res > INT_MAX // 10 or (res == INT_MAX // 10 and digit > 7):
-                return INT_MAX if sign == 1 else INT_MIN
-                
-            res = res * 10 + digit
+        elif s[0] == '+':
             i += 1
-            
+
+        while i < len(s) and s[i].isdigit():
+            res = res * 10 + int(s[i])
+
+            # Handle overflow
+            if sign * res > 2**31 - 1:
+                return 2**31 - 1
+            if sign * res < -2**31:
+                return -2**31
+
+            i += 1
+
         return sign * res
