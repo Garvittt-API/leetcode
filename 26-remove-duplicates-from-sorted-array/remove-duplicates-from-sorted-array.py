c82e5972062f4d5d033a69746bee28427486bb17
Garvittt.exe <garvitchoudhary2315@gmail.com>
2026-06-28 22:02:46 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (42.05%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -2,12 +2,13 @@ class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
         if not nums:
             return 0
-
+            
         write_ptr = 1
-        
-        for read_ptr in range(1, len(nums)):
-            if nums[read_ptr] != nums[read_ptr - 1]:
-                nums[write_ptr] = nums[read_ptr]
-                write_ptr += 1
+        n = len(nums)
 
+        for i in range(1, n):
+            if nums[i] != nums[i-1]:
+                nums[write_ptr] = nums[i]
+                write_ptr += 1
+                
         return write_ptr
