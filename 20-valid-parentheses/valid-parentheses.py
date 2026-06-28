5e2a2ab75826dafc9722c0c3abe8e9ef41e3d959
Garvittt.exe <garvitchoudhary2315@gmail.com>
2026-06-28 14:24:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (65.99%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,15 +1,15 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        mapping = {')': '(', '}': '{', ']': '['}
         
         for char in s:
-            if char in mapping:
-                top_element = stack.pop() if stack else '#'
-
-                if mapping[char] != top_element:
-                    return False
-            else:
-                stack.append(char)
+            if char == '(':
+                stack.append(')')
+            elif char == '{':
+                stack.append('}')
+            elif char == '[':
+                stack.append(']')
+            elif not stack or stack.pop() != char:
+                return False
 
         return not stack
