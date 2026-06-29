98e5c801960df8c4ec5f72800dc22280e7efe126
Garvittt.exe <garvitchoudhary2315@gmail.com>
2026-06-29 13:46:16 +0530

    Time: 26 ms (60.08%) | Memory: 20.1 MB (12.60%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
@@ -1,40 +1,47 @@
+from collections import Counter
+
 class Solution:
-    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+    def findSubstring(self, s: str, words: list[str]) -> list[int]:
         if not s or not words:
             return []
         
-        word_count = Counter(words)
-        n, k = len(words), len(words[0])
+        word_len = len(words[0])
+        num_words = len(words)
+        total_len = word_len * num_words
         s_len = len(s)
-        ans = []
         
-        # Check for each offset
-        for i in range(k):
+        word_count = Counter(words)
+        res = []
+        
+        # Iterate over each possible offset
+        for i in range(word_len):
             left = i
-            current_counts = Counter()
+            right = i
+            curr_count = Counter()
             words_found = 0
             
-            for right in range(i, s_len - k + 1, k):
-                word = s[right : right + k]
+            # Slide the window by word_len chunks
+            while right + word_len <= s_len:
+                word = s[right : right + word_len]
+                right += word_len
                 
                 if word in word_count:
-                    current_counts[word] += 1
+                    curr_count[word] += 1
                     words_found += 1
                     
-                    # If we have too many of one word, shrink from the left
-                    while current_counts[word] > word_count[word]:
-                        left_word = s[left : left + k]
-                        current_counts[left_word] -= 1
+                    # Shrink if frequency exceeds required count
+                    while curr_count[word] > word_count[word]:
+                        left_word = s[left : left + word_len]
+                        curr_count[left_word] -= 1
                         words_found -= 1
-                        left += k
+                        left += word_len
                     
-                    # Check if we have found all words
-                    if words_found == n:
-                        ans.append(left)
+                    if words_found == num_words:
+                        res.append(left)
                 else:
-                    # Reset if word is not in the list
-                    current_counts.clear()
+                    # Reset window if word is not in target list
+                    curr_count.clear()
                     words_found = 0
-                    left = right + k
-        
-        return ans
+                    left = right
+                    
+        return res
