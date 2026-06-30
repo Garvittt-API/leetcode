c33dd400f81a0bb07db5c962b73718c2a6fa33d7
Garvittt.exe <garvitchoudhary2315@gmail.com>
2026-07-01 03:47:38 +0530

    Time: 3 ms (52.93%) | Memory: 55.1 MB (62.74%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -3,15 +3,19 @@ public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
 
-        for (int i = 0; i < n; i++) {
-            while (nums[i] >= 1 &&
-                   nums[i] <= n &&
-                   nums[nums[i] - 1] != nums[i]) {
-                swap(nums[i], nums[nums[i] - 1]);
+        for (int i = 0; i < n; ++i) {
+            while (true) {
+                int x = nums[i];
+
+                if (x < 1 || x > n) break;
+
+                if (nums[x - 1] == x) break;
+
+                swap(nums[i], nums[x - 1]);
             }
         }
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; ++i) {
             if (nums[i] != i + 1)
                 return i + 1;
         }
