cb8aba552425cb1f4694156d90621db8739eb15e
Garvittt.exe <garvitchoudhary2315@gmail.com>
2026-06-30 16:23:26 +0530

    Time: 23 ms (98.08%) | Memory: 19.5 MB (81.13%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.py
+++ 37-sudoku-solver/sudoku-solver.py
@@ -1,39 +1,66 @@
 class Solution:
     def solveSudoku(self, board: list[list[str]]) -> None:
-        rows = [set() for _ in range(9)]
-        cols = [set() for _ in range(9)]
-        boxes = [set() for _ in range(9)]
-        
-        # Pre-fill sets with existing numbers
+        # Bitmasks for rows, columns, and 3x3 boxes
+        rows = [0] * 9
+        cols = [0] * 9
+        boxes = [0] * 9
+        empty = []
+
+        def get_idx(r, c): return (r // 3) * 3 + (c // 3)
+
+        # Pre-fill bitmasks
         for r in range(9):
             for c in range(9):
                 if board[r][c] != '.':
-                    val = board[r][c]
-                    rows[r].add(val)
-                    cols[c].add(val)
-                    boxes[(r // 3) * 3 + (c // 3)].add(val)
-        
+                    val = int(board[r][c]) - 1
+                    mask = 1 << val
+                    rows[r] |= mask
+                    cols[c] |= mask
+                    boxes[get_idx(r, c)] |= mask
+                else:
+                    empty.append((r, c))
+
+        def get_possibilities(r, c):
+            # Combined mask of used numbers in row, col, and box
+            used = rows[r] | cols[c] | boxes[get_idx(r, c)]
+            return ~used & 0x1FF  # All 1s for available spots
+
         def solve():
-            for r in range(9):
-                for c in range(9):
-                    if board[r][c] == '.':
-                        for val in '123456789':
-                            b_idx = (r // 3) * 3 + (c // 3)
-                            if val not in rows[r] and val not in cols[c] and val not in boxes[b_idx]:
-                                # Place
-                                board[r][c] = val
-                                rows[r].add(val)
-                                cols[c].add(val)
-                                boxes[b_idx].add(val)
-                                
-                                if solve(): return True
-                                
-                                # Backtrack
-                                board[r][c] = '.'
-                                rows[r].remove(val)
-                                cols[c].remove(val)
-                                boxes[b_idx].remove(val)
-                        return False
-            return True
+            if not empty: return True
+            
+            # Heuristic: Pick the cell with fewest possibilities
+            best_idx = -1
+            min_count = 10
+            best_poss = 0
+            
+            for i, (r, c) in enumerate(empty):
+                poss = get_possibilities(r, c)
+                count = bin(poss).count('1')
+                if count == 0: return False
+                if count < min_count:
+                    min_count = count
+                    best_idx = i
+                    best_poss = poss
+                    if count == 1: break # Cannot do better than 1
+            
+            r, c = empty.pop(best_idx)
+            for val in range(9):
+                if (best_poss >> val) & 1:
+                    mask = 1 << val
+                    board[r][c] = str(val + 1)
+                    rows[r] |= mask
+                    cols[c] |= mask
+                    boxes[get_idx(r, c)] |= mask
+                    
+                    if solve(): return True
+                    
+                    # Backtrack
+                    board[r][c] = '.'
+                    rows[r] &= ~mask
+                    cols[c] &= ~mask
+                    boxes[get_idx(r, c)] &= ~mask
+            
+            empty.insert(best_idx, (r, c))
+            return False
 
         solve()
