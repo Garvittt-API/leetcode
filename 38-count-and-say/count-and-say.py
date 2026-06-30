f7f0f0442941bdcc93160257a5d14ffd932fc4fe
Garvittt.exe <garvitchoudhary2315@gmail.com>
2026-06-30 16:29:53 +0530

    Time: 7 ms (76.37%) | Memory: 19.3 MB (36.43%) - LeetSync

--- 38-count-and-say/count-and-say.py
+++ 38-count-and-say/count-and-say.py
@@ -1,18 +1,23 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
+        # Pre-defining strings for small counts to avoid repeated str() calls
+        # This reduces overhead significantly
+        counts = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        
         res = "1"
         for _ in range(n - 1):
             next_res = []
-            length = len(res)
             i = 0
-            while i < length:
+            n_res = len(res)
+            while i < n_res:
+                char = res[i]
                 count = 1
-                # Use a simple pointer scan to avoid list/iterator overhead
-                while i + 1 < length and res[i] == res[i + 1]:
-                    i += 1
+                while i + 1 < n_res and res[i + 1] == char:
                     count += 1
-                next_res.append(str(count))
-                next_res.append(res[i])
+                    i += 1
+                # Use the pre-defined counts and avoid str() function calls
+                next_res.append(counts[count])
+                next_res.append(char)
                 i += 1
             res = "".join(next_res)
         return res
