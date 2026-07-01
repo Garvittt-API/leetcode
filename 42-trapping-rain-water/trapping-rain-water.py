1a65030dc863e4c6897f33c87ff5c542056e776f
Garvittt.exe <garvitchoudhary2315@gmail.com>
2026-07-01 17:42:31 +0530

    Time: 3 ms (95.05%) | Memory: 21.2 MB (11.14%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,21 +1,23 @@
 class Solution:
     def trap(self, height: list[int]) -> int:
-        if not height:
-            return 0
+        if not height: return 0
         
         left, right = 0, len(height) - 1
         left_max, right_max = height[left], height[right]
         water = 0
         
         while left < right:
-            # Move the pointer pointing to the smaller height
             if left_max < right_max:
                 left += 1
-                left_max = max(left_max, height[left])
-                water += left_max - height[left]
+                # Only calculate if current height is less than the max
+                if height[left] < left_max:
+                    water += left_max - height[left]
+                else:
+                    left_max = height[left]
             else:
                 right -= 1
-                right_max = max(right_max, height[right])
-                water += right_max - height[right]
-                
+                if height[right] < right_max:
+                    water += right_max - height[right]
+                else:
+                    right_max = height[right]
         return water
