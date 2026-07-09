9d96fdb6a1c08c3a832d920c1e796974636178cc
Garvittt.exe <garvitchoudhary2315@gmail.com>
2026-07-10 00:03:46 +0530

    Time: 69 ms (80.95%) | Memory: 50.1 MB (57.94%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.py
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.py
@@ -1,14 +1,14 @@
 class Solution:
     def pathExistenceQueries(self, n: int, nums: list[int], maxDiff: int, queries: list[list[int]]) -> list[bool]:
-        # group[i] will store the ID of the connected component node i belongs to
+        # Pre-allocate array for fast localized cache lookups
         group = [0] * n
-        component_id = 0
+        curr = 0
         
-        # Identify the contiguous connected components
-        for i in range(1, n):
-            if nums[i] - nums[i - 1] > maxDiff:
-                component_id += 1
-            group[i] = component_id
+        # Using a zipped loop is faster in Python than range indexing
+        for i, (prev_val, curr_val) in enumerate(zip(nums, nums[1:]), 1):
+            if curr_val - prev_val > maxDiff:
+                curr += 1
+            group[i] = curr
             
-        # Answer each query instantly by comparing component IDs
+        # Localize the array lookup to speed up the list comprehension
         return [group[u] == group[v] for u, v in queries]
