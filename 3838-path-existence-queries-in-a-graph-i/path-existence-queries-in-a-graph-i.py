717984b6b515f6ef7352a74fbfd22a24d3de7081
Garvittt.exe <garvitchoudhary2315@gmail.com>
2026-07-10 00:04:17 +0530

    Time: 80 ms (69.84%) | Memory: 49.4 MB (80.16%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.py
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.py
@@ -1,14 +1,15 @@
 class Solution:
     def pathExistenceQueries(self, n: int, nums: list[int], maxDiff: int, queries: list[list[int]]) -> list[bool]:
-        # Pre-allocate array for fast localized cache lookups
+        # Pre-allocate array for fast, contiguous memory layout
         group = [0] * n
         curr = 0
         
-        # Using a zipped loop is faster in Python than range indexing
-        for i, (prev_val, curr_val) in enumerate(zip(nums, nums[1:]), 1):
-            if curr_val - prev_val > maxDiff:
+        # Micro-optimization: standard for-loops with local vars are fastest for mutating elements
+        for i in range(1, n):
+            if nums[i] - nums[i - 1] > maxDiff:
                 curr += 1
             group[i] = curr
             
-        # Localize the array lookup to speed up the list comprehension
-        return [group[u] == group[v] for u, v in queries]
+        # Using map with a lambda bypasses the byte-code execution loop of a list comprehension,
+        # executing the loop natively in C speed.
+        return list(map(lambda q: group[q[0]] == group[q[1]], queries))
